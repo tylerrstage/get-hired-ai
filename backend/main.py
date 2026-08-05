e2b470bb08132ddc91614d0f3d377171d39c4e28
tylerrstage <tyler.stageberg@gmail.com>
2026-08-05 07:27:43 -0500

    Implement PDF text extraction and update requirements

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import File, Form, UploadFile
+from pdf_parser import extract_text_from_pdf
 
+
+# Every item (missing keyword) needs more than a single value.
 class MissingKeyword(BaseModel):
     label: str
     variant: str # "danger" or "info"
 
+# Enforces shape. Ensures that every variable is of a specific type, else will raise an error.
 class AnalysisResponse(BaseModel):
     score: int
     score_max: int
@@ -21,6 +25,8 @@ app = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 
+# Since Vite and FastAPI run on different ports, the browser, by default, blocks them from making fetch requests to one another.
+# This code intercepts every response and allows all HTTP methods (GET, POST, etc) and request headers to run from one origin to another.
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ["http://localhost:5173"],
@@ -28,15 +34,17 @@ app.add_middleware(
     allow_headers = ["*"],
 )
 
+# When a GET request arrives at the URL path ("/"), read_root is called.
 @app.get("/")
 def read_root():
     return {"status": "ok"}
 
+# Takes resume from user input, calls pdf_parser to extract text, and returns an analysis response.
 @app.post("/analyze", response_model = AnalysisResponse)
 async def analyze(resume: UploadFile = File(...), job_description: str = Form(...)):
     contents = await resume.read()
-    print(f"Received file: {resume.filename}, {len(contents)} bytes")
-    print(f"Job description length: {len(job_description)} characters")
+    resume_text = extract_text_from_pdf(contents)
+    print(resume_text)
 
     return AnalysisResponse(
         score = 78,
